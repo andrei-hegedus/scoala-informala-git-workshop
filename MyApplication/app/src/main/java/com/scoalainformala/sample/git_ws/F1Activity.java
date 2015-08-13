a7cb6854dd48a604304fbb263229022427ec2f07
unknown <rtomos@EN63117.endava.net>
2015-08-13 21:01:48 +0300

    Smth

---
+++ MyApplication/app/src/main/java/com/scoalainformala/sample/git_ws/F1Activity.java
@@ -0,0 +1,10 @@
+package com.scoalainformala.sample.git_ws;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by andrei on 8/13/15.
+ */
+public class F1Activity extends AppCompatActivity {
+
+}
