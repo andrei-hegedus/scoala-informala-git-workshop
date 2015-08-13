679c630c9a821b22e8455fdd8d93b91fb077920e
unknown <rtomos@EN63117.endava.net>
2015-08-13 20:21:56 +0300

    F5 - Implemented F5Activity.

---
+++ MyApplication/app/src/main/java/com/scoalainformala/sample/git_ws/F5Activity.java
@@ -0,0 +1,9 @@
+package com.scoalainformala.sample.git_ws;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rtomos on 8/13/2015.
+ */
+public class F5Activity extends AppCompatActivity {
+}
